b5081c9111457fed41a0d7ed5f8d9e61859476a2
Tarun Purohit <tarunpurohit2903@gmail.com>
2026-05-24 14:00:58 +0530

    fixed cron job issue

--- server/server.js
+++ server/server.js
@@ -22,8 +22,10 @@ app.get("/", (req, res) => {
 });
 // Start Cron Job
 const cron = require("node-cron");
-cron.schedule("0 0 * * *", processFeedbacksCron); // Runs daily at midnight
+cron.schedule("0 * * * *", processFeedbacksCron); // Runs every hour
 
 app.listen(PORT, () => {
   console.log(`server running at port ${PORT}`);
+  // Run on startup to catch any missed jobs while server was asleep
+  processFeedbacksCron();
 });
